b0f36b900e7a60132e4030f9914e3cc9bdb79799
zulfanandria <zulfan.andria@gmail.com>
2022-07-13 22:07:47 +0800

    1st

---
+++ Kalkulator.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <stack>
+#include <math.h>
+#include <string>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string InPostfix;
+
+int Operator(char Hitung)
+{
+    if (Hitung == '^')
+    {
+        return 3;
+    }
+    else if (Hitung == '/' || Hitung == '*')
+    {
+        return 2;
+    }
+    else if (Hitung == '+' || Hitung == '-')
+    {
+        return 1;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+void InfixKePostfix(string input)
+{
+    stack<char> iStack;
+    for (int i = 0; i < input.length(); i++)
+    {
+        char Hitung = input[i];
+        if (Hitung >= '0' && Hitung <= '9')
+            InPostfix += Hitung;
+        else if (Hitung == '(')
+            iStack.push('(');
+        else if (Hitung == ')')
+        {
+            while (iStack.top() != '(')
+            {
+                InPostfix += iStack.top();
+                iStack.pop();
+            }
+            iStack.pop();
+        }
+        else
+        {
+            while (!iStack.empty() && Operator(input[i]) <= Operator(iStack.top()))
+            {
+                InPostfix += iStack.top();
+                iStack.pop();
+            }
+            iStack.push(Hitung);
+        }
+    }
+    while (!iStack.empty())
+    {
+        InPostfix += iStack.top();
+        iStack.pop();
+    }
+}
+
+int CalcuPostfix(string input)
+{
+    stack<int> jStack;
+    int pjg = input.length();
+    for (int j = 0; j < pjg; j++)
+    {
+        if (input[j] >= '0' && input[j] <= '9')
+        {
+            jStack.push(input[j] - '0');
+        }
+        else
+        {
+            int Num1 = jStack.top();
+            jStack.pop();
+            int Num2 = jStack.top();
+            jStack.pop();
+            if (input[j] == '+')
+            {
+                jStack.push(Num1 + Num2);
+            }
+            else if (input[j] == '-')
+            {
+                jStack.push(Num2 - Num1);
+            }
+            else if (input[j] == '/')
+            {
+                jStack.push(Num2 / Num1);
+            }
+            else if (input[j] == '*')
+            {
+                jStack.push(Num2 * Num1);
+            }
+            else if (input[j] == '^')
+            {
+                jStack.push(pow(Num2, Num1));
+            }
+        }
+    }
+    return jStack.top();
+}
+
+int main()
+{
+    string input;
+    getline(cin, input);
+
+    // Menghilangkan spasi menjadi bentuk Infix
+    int len = input.length();
+    int nSpace = count(input.begin(), input.end(), ' ');
+    remove(input.begin(), input.end(), ' ');
+    input.resize(len - nSpace);
+
+    // Mengubah Infix menjadi Postfix
+    InfixKePostfix(input);
+    
+    // Menghitung Postfix
+    input = InPostfix;
+    CalcuPostfix(input);
+    cout << CalcuPostfix(input);
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Kalkulator-Sederhana
+
+Kalkulator dengan input seperti 1 + 2 + 3 * 4 . Semua angka dan operator dipisah oleh tanda spasi. Disini saya menggunakan 
+cara stack, namun sebelum itu saya membuat input tersebut agar dihilangkan tanda spasinya, 
+sehingga menjadi 1+2+3*4 . Cara tersebut saya gunakan dengan cara menghitung panjang 
+string, lalu mendeteksi tempat tanda spasi, dan kemudian dihapuskan. 
+
+ Setelah dihapuskan, bentuk tersebut Nampak seperti bentuk infix, dimana disini saya 
+mendapat ide bahwa bisa saja diubah ke bentuk postfix lalu dihitung dengan menggunakan 
+stack. Pertama tama, ubah bentuk infix ke postfix, sehingga dapat berbentuk menjadi 
+12+34*+ . Disitu artinya angka 1 dan 2 ditambahkan, dan angka 3 dan 4 dikalikan. 
+Setelah itu barulah hasil dari keduanya ditambahkan, sehingga didapatkan nilai 15 sebagai 
+hasilnya. 
